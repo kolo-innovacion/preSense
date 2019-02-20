0ff5047a64e36a2526f9ddf15b38d9e07637d3c3
alejandrothackerkolo <44816682+alejandrothackerkolo@users.noreply.github.com>
2019-02-20 14:30:06 -0600

    Add files via upload

---
+++ maxbot_nano/maxbot_nano.ino
@@ -0,0 +1,64 @@
+//kolo sensor maxbot.
+//Author: Eduardo Gante.
+// Date: 23/07/09
+// pin 11 entrada  
+// pin 9 salida
+
+
+const int pwPin = 11;
+const int ledPIN = 9;
+
+long pulse, inches, cm;
+
+void setup()
+
+{
+
+
+  Serial.begin(9600);
+
+}
+
+void loop()
+
+{
+
+ pinMode(pwPin, INPUT);
+ pinMode(ledPIN , OUTPUT); 
+
+
+  pulse = pulseIn(pwPin, HIGH);
+
+  //147uS per inch
+
+
+
+  
+
+  inches = pulse / 147;
+
+  //change inches to centimetres
+
+  cm = inches * 2.54;
+
+if (cm < 90){
+
+  digitalWrite(ledPIN , HIGH);
+
+  
+  Serial.print(cm);
+
+  Serial.print("cm");
+
+  Serial.println();
+
+}
+else
+{
+  digitalWrite(ledPIN , LOW);
+  }
+
+
+  delay(500);
+
+}
